3115e7ffffae5f568a183ca90faf48689dc93f6e
YuyoungRhee <bbclara@naver.com>
2024-11-11 02:25:03 +0900

    feat(OrderService): 주문 처리 및 검증 로직 구현
    - 주문 처리 및 검증을 담당하는 서비스 클래스
    - 주문에 대한 확인 및 검증 후, 주문을 체크하고 진행하여 결과를 반환하는 역할

---
+++ src/main/java/store/service/OrderService.java
@@ -0,0 +1,63 @@
+package store.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import store.controller.InputHandler;
+import store.domain.order.Order;
+import store.dto.OrderCheckDto;
+import store.dto.OrderResultDto;
+
+public class OrderService {
+    private final List<Order> orders;
+    private final InputHandler inputHandler;
+
+    public OrderService(List<Order> orders, InputHandler inputHandler) {
+        this.orders = orders;
+        this.inputHandler = inputHandler;
+    }
+
+    public List<OrderResultDto> processOrders() {
+        List<OrderResultDto> orderResults = new ArrayList<>();
+        for (Order order : orders) {
+            OrderResultDto orderResult = checkAndProcessSingleOrder(order);
+            orderResults.add(orderResult);
+        }
+        return orderResults;
+    }
+
+    private OrderResultDto checkAndProcessSingleOrder(Order order) {
+        while(true) {
+            OrderCheckDto orderCheckDto = order.checkOrder();
+
+            if (orderCheckDto.canProceedOrder()) {
+                break;
+            }
+
+            requestAdditionalInput(orderCheckDto, order);
+        }
+        return order.processOrder();
+    }
+
+    private void requestAdditionalInput(OrderCheckDto orderCheckDto, Order order) {
+        if (!orderCheckDto.isEnough()) {
+            System.out.println("[ERROR] 재고 수량을 초과하여 구매할 수 없습니다. 다시 입력해 주세요.");
+            inputHandler.getOrders();
+        }
+
+        if (orderCheckDto.getAvailableGiftQuantity() > 0) {
+            boolean addGift = inputHandler.askForAdditionalGift(orderCheckDto.getProductName(),
+                    orderCheckDto.getAvailableGiftQuantity());
+            if (addGift) {
+                order.addAvailableGiftQuantity(orderCheckDto.getAvailableGiftQuantity());
+            }
+        }
+
+        if (orderCheckDto.getNoPromotionQuantity() > 0) {
+            boolean confirmRegularPrice = inputHandler.askForNoPromotion(orderCheckDto.getProductName(),
+                    orderCheckDto.getNoPromotionQuantity());
+            if (confirmRegularPrice) {
+                order.excludeNoPromotionQuantity(orderCheckDto.getNoPromotionQuantity());
+            }
+        }
+    }
+}
