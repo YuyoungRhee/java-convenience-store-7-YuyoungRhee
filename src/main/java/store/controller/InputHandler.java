b50f008864c7550e1c730de0407daa55fc82f9b7
YuyoungRhee <bbclara@naver.com>
2024-11-11 02:23:49 +0900

    feat(InputHandler): 주문 입력 및 추가 선택 사항 처리 클래스 구현
    - 사용자 입력을 처리하고 주문을 생성하는 역할
    - 주문 목록을 받아오고, 프로모션과 추가 기프트 관련 선택 사항을 처리하는 기능

---
+++ src/main/java/store/controller/InputHandler.java
@@ -0,0 +1,71 @@
+package store.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import store.domain.order.Order;
+import store.domain.order.OrderFactory;
+import store.view.InputView;
+
+public class InputHandler {
+    private final InputView inputView;
+    private final OrderFactory orderFactory;
+
+    public InputHandler(InputView inputView, OrderFactory orderFactory) {
+        this.inputView = inputView;
+        this.orderFactory = orderFactory;
+    }
+
+    public List<Order> getOrders() {
+        String orderInput = inputView.readOrders();
+        //검증로직 추가 필요 예외처리도
+        List<Order> orders = new ArrayList<>();
+        String[] orderItems = orderInput.split(",");
+
+        for (String item : orderItems) {
+            item = item.trim();
+            String[] parts = item.split("-");
+            if (parts.length == 2) {
+                String productName = parts[0].substring(1); // 대괄호 제거
+                int quantity = Integer.parseInt(parts[1].substring(0, parts[1].length() - 1)); // 수량 추출
+                System.out.println("productName = " + productName);
+                orders.add(orderFactory.createOrder(productName, quantity));
+            }
+        }
+        return orders;
+    }
+    public boolean askForNoPromotion(String productName, int noPromotionQuantity) {
+        String prompt = String.format(
+                "현재 %s %d개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)",
+                productName, noPromotionQuantity
+        );
+        String input = inputView.readAnswerForNoPromotion(prompt);
+        //검증로직
+
+        return input.equals("Y");
+
+    }
+    public boolean askForAdditionalGift(String productName, int additionalGift) {
+        String prompt = String.format(
+                "현재 %s은(는) %d개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)",
+                productName, additionalGift
+        );
+        String input = inputView.readAnswerForAdditionalGift(prompt);
+        //검증로직
+
+        return input.equals("Y");
+    }
+
+    public boolean askApplyDiscount() {
+        String input = inputView.readAnswerForApplyDiscount();
+        //검증 로직
+
+        return input.equals("Y");
+    }
+
+    public boolean askForAdditionalPurchase() {
+        String input = inputView.readAnswerForAdditionalPurchase();
+        //검증 로직
+
+        return input.equals("Y");
+    }
+}
