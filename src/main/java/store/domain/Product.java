08bbb2924c75b9fd0625488343b80ce760995563
YuyoungRhee <bbclara@naver.com>
2024-11-11 02:13:43 +0900

    feat(product): 상품 클래스 및 기능 구현
    - Product 클래스: 상품의 가격 계산, 재고 관리 및 프로모션에 따른 할인/증정 수량 처리

---
+++ src/main/java/store/domain/Product.java
@@ -0,0 +1,62 @@
+package store.domain;
+
+import store.domain.promotion.Promotion;
+
+public class Product {
+    private final String name;
+    private final int price;
+    private int quantity;
+    private final Promotion promotion;
+
+    public Product(String name, int price, int quantity, Promotion promotion) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.promotion = promotion;
+    }
+
+    public int calculateTotalPrice(int quantity) {
+        return price * quantity;
+    }
+
+    public void reduceQuantity(int requestQuantity) {
+        //예외처리?
+        int reducedQuantity = Math.min(quantity, requestQuantity);
+
+        quantity -= reducedQuantity;
+    }
+
+    public int getPromotionQuantity(int requestQuantity) {
+        if(promotion.isSatisfiedBy()) {
+            return Math.min(requestQuantity, quantity);
+        }
+        return 0;
+    }
+
+    public int calculateDiscountedPrice(int requestQuantity) {
+        if (promotion.isSatisfiedBy()) {
+            return 0;
+        }
+        int giftQuantity = promotion.calculateGiftQuantity(requestQuantity);
+        int payableQuantity = requestQuantity - giftQuantity;
+
+        return price * payableQuantity;
+    }
+
+    public int calculateGiftQuantity(int requestQuantity) {
+        return promotion.calculateGiftQuantity(requestQuantity);
+    }
+
+    public int calculateAdditionalGiftQuantity(int requestedQuantity) {
+        return promotion.calculateAvailableGiftQuantity(requestedQuantity);
+    }
+
+    public int excludeDiscountQuantity(int requestedQuantity) {
+        return promotion.excludeDiscountQuantity(requestedQuantity);
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+}
